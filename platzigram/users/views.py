1c38ad8234d256a610c8a073d0f3d2eaebd28a50
Franco Manca <francomanca93@gmail.com>
2020-11-24 10:15:35 +0100

    Funcion signup. Sign up view. Intermediario

--- platzigram/users/views.py
+++ platzigram/users/views.py
@@ -7,18 +7,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 
-# Exceptions
-# Importamos posible error al tratar de crear
-# una instancia con valor único que ya existe
-from django.db.utils import IntegrityError
-
-# Models
-# Importamos los modelos de las instancias que crearemos
-from django.contrib.auth.models import User
-from users.models import Profile
-
 # Forms
-from users.forms import ProfileForm
+from users.forms import ProfileForm, SignupForm
 
 # Create your views here.
 
@@ -71,41 +61,21 @@ def login_view(request):
 
 
 def signup(request):
-    """ Sign up view."""
-    if request.method == 'POST':
-        email = request.POST['email']
-        username = request.POST['username']
-        passwd = request.POST['passwd']
-        passwd_confirmation = request.POST['passwd_confirmation']
+    """ Sign up view.
+    Intermediario"""
+    if request.method == "POST":
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = SignupForm()
 
-        # PASSWORD VALIDATION
-        if passwd != passwd_confirmation:
-            error = 'The passwords do not match.'
-            return render(request, 'users/signup.html', {'error': error})
-
-        # EMAIL VALIDATION
-        email_validation = User.objects.filter(email=email)
-        if email_validation:
-            error = f'There is another account using {email}'
-            return render(request, 'users/signup.html', {'error': error})
-
-        # USERNAME VALIDATION
-        try:
-            user = User.objects.create_user(username=username, password=passwd)
-            user.first_name = request.POST['first_name']
-            user.last_name = request.POST['last_name']
-            user.email = email
-            user.save()
-        except IntegrityError:
-            return render(request, 'users/signup.html',
-                          {'error': 'Username is already exist'})
-
-        profile = Profile(user=user)
-        profile.save()
-
-        return redirect('login')
-
-    return render(request, 'users/signup.html')
+    return render(
+        request=request,
+        template_name='users/signup.html',
+        context={'form': form}
+    )
 
 
 @login_required
